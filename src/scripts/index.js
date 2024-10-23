a5846ca2ffbcb67563f6524e2e0d41f3fe3f4051
Welington Silva <welingtonsilva401@gmail.com>
2024-10-23 06:58:50 -0300

    criação da lógica de contador de tempo, melhoria nos estilos e padronização do botões

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,15 +2,56 @@ let currentQuestionIndex = 0;
 let score = 0;
 let helpUsed = false;
 let shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+let timerInterval;
+let countdowmn;
 
 const shuffle = (array) => {
   array.sort(() => Math.random() - 0.5);
 };
 
-const checkAnswer = (isCorrect) => {
+const startTimer = () => {
+  const timerElement = document.getElementById("timer");
+  countdowmn = 10;
+
+  timerInterval = setInterval(() => {
+    countdowmn--;
+    timerElement.textContent = countdowmn;
+
+    if (countdowmn === 0) {
+      clearInterval(timerInterval);
+      handleTimeout();
+    }
+  }, 1000);
+};
+
+const handleTimeout = () => {
+  const optionsContainer = document.getElementById("options-container");
+  const feedbackContainer = document.getElementById("feedback-container");
+  const feedback = document.getElementById("feedback");
+
+  feedbackContainer.style.display = "flex";
+  feedbackContainer.style.backgroundColor = "rgb(128,0,0)";
+  optionsContainer.style.display = "none";
+
+  feedback.textContent = "Tempo esgotado! Seja mais rápido na próxima.";
+
+  const nextButton = document.getElementById("next-button");
+  nextButton.style.display = "inline";
+  document.getElementById("help-button").style.display = "none";
+};
+
+const checkAnswer = (isCorrect, clickedOption) => {
+  clearInterval(timerInterval);
   const feedback = document.getElementById("feedback");
   const nextButton = document.getElementById("next-button");
   const scoreDisplay = document.getElementById("score");
+  const helpButton = document.getElementById("help-button");
+
+  const optionsContainer = document.getElementById("options-container");
+  const feedbackContainer = document.getElementById("feedback-container");
+
+  feedbackContainer.style.display = "flex";
+  optionsContainer.style.display = "none";
 
   if (isCorrect) {
     if (helpUsed) {
@@ -21,29 +62,30 @@ const checkAnswer = (isCorrect) => {
       feedback.textContent = "Resposta correta! Parabéns!";
       score++;
     }
-    feedback.style.color = "green";
+    feedbackContainer.style.backgroundColor = "rgb(0, 128, 0)";
   } else {
     feedback.textContent = "Resposta errada! Talvez na próxima.";
-    feedback.style.color = "red";
+    feedbackContainer.style.backgroundColor = "rgb(128, 0, 0)";
   }
 
   scoreDisplay.textContent = `Sua pontuação: ${score * 10}`;
 
   const options = document.querySelectorAll(".option");
-  options.forEach((option) => {
+
+  options.forEach((option, index) => {
     option.disabled = true;
-    option.style.opacity = 0.5;
 
     option.style.cursor = "default";
 
-    if (isCorrect) {
-      option.style.backgroundColor = "green";
-    } else {
-      option.style.backgroundColor = "red";
+    if (option === clickedOption && isCorrect) {
+      option.style.backgroundColor = "rgb(0, 128, 0)";
+    } else if (!isCorrect || !option.correct) {
+      option.style.backgroundColor = "rgb(128, 0, 0)";
     }
   });
 
   nextButton.style.display = "inline";
+  helpButton.style.display = "none";
 };
 
 const useHelp = () => {
@@ -70,6 +112,9 @@ const displayQuestion = () => {
   const feedback = document.getElementById("feedback");
   const nextButton = document.getElementById("next-button");
   const helpButton = document.getElementById("help-button");
+  const feedbackContainer = document.getElementById("feedback-container");
+
+  feedbackContainer.style.display = "none";
 
   feedback.textContent = "";
   optionsContainer.innerHTML = "";
@@ -87,12 +132,19 @@ const displayQuestion = () => {
     button.classList.add("option");
     button.textContent = `(${String.fromCharCode(65 + index)}) ${option.text}`;
 
-    button.onclick = () => checkAnswer(option.correct);
+    button.correct = option.correct;
+
+    button.onclick = () => checkAnswer(option.correct, button);
     optionsContainer.appendChild(button);
   });
+  startTimer();
 };
 
 const nextQuestion = () => {
+  const optionsContainer = document.getElementById("options-container");
+
+  optionsContainer.style.display = "grid";
+
   currentQuestionIndex++;
   if (currentQuestionIndex < shuffledQuestions.length) {
     displayQuestion();
@@ -136,7 +188,7 @@ function restartQuiz() {
   shuffledQuestions = [...questions];
   shuffle(shuffledQuestions);
 
-  document.getElementById("options-container").style.display = "block";
+  document.getElementById("options-container").style.display = "gap";
   document.getElementById("help-button").style.display = "inline";
   document.getElementById("restart-button").style.display = "none";
 
@@ -145,5 +197,4 @@ function restartQuiz() {
   displayQuestion();
 }
 
-// Inicializa o quiz
 displayQuestion();
--- src/scripts/questions.js
+++ src/scripts/questions.js
@@ -1,60 +1,56 @@
-// Arquivo questions.js
-// Definindo o array de perguntas no escopo global
-
 const questions = [
-    {
-      question: "Qual dos seguintes objetivos faz parte dos ODS?",
-      options: [
-        { text: "Colonização de Marte", correct: false },
-        { text: "Erradicação da Pobreza", correct: true },
-        { text: "Viagens interplanetárias", correct: false },
-        { text: "Realização de shows musicais no espaço", correct: false },
-      ],
-    },
-    {
-      question: "Qual ODS está relacionado à Igualdade de Gênero?",
-      options: [
-        { text: "ODS 3", correct: false },
-        { text: "ODS 5", correct: true },
-        { text: "ODS 9", correct: false },
-        { text: "ODS 15", correct: false },
-      ],
-    },
-    {
-      question: "Qual ODS trata de Ação contra a Mudança Global do Clima?",
-      options: [
-        { text: "ODS 1", correct: false },
-        { text: "ODS 13", correct: true },
-        { text: "ODS 7", correct: false },
-        { text: "ODS 12", correct: false },
-      ],
-    },
-    {
-      question: "Qual ODS trata da Vida Terrestre?",
-      options: [
-        { text: "ODS 11", correct: false },
-        { text: "ODS 14", correct: false },
-        { text: "ODS 15", correct: true },
-        { text: "ODS 6", correct: false },
-      ],
-    },
-    {
-      question: "Qual ODS trata do Consumo e Produção Responsáveis?",
-      options: [
-        { text: "ODS 8", correct: false },
-        { text: "ODS 12", correct: true },
-        { text: "ODS 9", correct: false },
-        { text: "ODS 7", correct: false },
-      ],
-    },
-    {
-      question: "Qual ODS está relacionado à Educação de Qualidade?",
-      options: [
-        { text: "ODS 2", correct: false },
-        { text: "ODS 3", correct: false },
-        { text: "ODS 4", correct: true },
-        { text: "ODS 6", correct: false },
-      ],
-    },
-  ];
-  
+  {
+    question: "Qual dos seguintes objetivos faz parte dos ODS?",
+    options: [
+      { text: "Colonização de Marte", correct: false },
+      { text: "Erradicação da Pobreza", correct: true },
+      { text: "Viagens interplanetárias", correct: false },
+      { text: "Realização de shows musicais no espaço", correct: false },
+    ],
+  },
+  {
+    question: "Qual ODS está relacionado à Igualdade de Gênero?",
+    options: [
+      { text: "ODS 3", correct: false },
+      { text: "ODS 5", correct: true },
+      { text: "ODS 9", correct: false },
+      { text: "ODS 15", correct: false },
+    ],
+  },
+  {
+    question: "Qual ODS trata de Ação contra a Mudança Global do Clima?",
+    options: [
+      { text: "ODS 1", correct: false },
+      { text: "ODS 13", correct: true },
+      { text: "ODS 7", correct: false },
+      { text: "ODS 12", correct: false },
+    ],
+  },
+  {
+    question: "Qual ODS trata da Vida Terrestre?",
+    options: [
+      { text: "ODS 11", correct: false },
+      { text: "ODS 14", correct: false },
+      { text: "ODS 15", correct: true },
+      { text: "ODS 6", correct: false },
+    ],
+  },
+  {
+    question: "Qual ODS trata do Consumo e Produção Responsáveis?",
+    options: [
+      { text: "ODS 8", correct: false },
+      { text: "ODS 12", correct: true },
+      { text: "ODS 9", correct: false },
+      { text: "ODS 7", correct: false },
+    ],
+  },
+  {
+    question: "Qual ODS está relacionado à Educação de Qualidade?",
+    options: [
+      { text: "ODS 2", correct: false },
+      { text: "ODS 3", correct: false },
+      { text: "ODS 4", correct: true },
+      { text: "ODS 6", correct: false },
+    ],
+  },
+];
